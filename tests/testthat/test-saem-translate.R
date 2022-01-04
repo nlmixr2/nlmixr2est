f83b60f33d6eb83da8361774572fd67d85ff8b50
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-01-03 21:13:27 -0600

    Add a non-mu referenced example with fixed theta and omegas

--- tests/testthat/test-saem-translate.R
+++ tests/testthat/test-saem-translate.R
@@ -1,5 +1,6 @@
 nlmixr2Test({
-  test_that("theo tests", {
+
+  test_that("Standard theo linCmt()", {
 
     one.cmt <- function() {
       ini({
@@ -102,6 +103,113 @@ nlmixr2Test({
 
     expect_equal(f$saemThetaDataFrame,
                  structure(list(lower = c(-Inf, -Inf, -Inf, -Inf), theta = c(0.45, 0.993251773010283, 3.45, 0.7), fixed = c(FALSE, FALSE, FALSE, FALSE), upper = c(Inf, Inf, Inf, Inf)), class = "data.frame", row.names = c("tka", "tcl", "tv", "add.sd")))
+
+
+    test_that("non mu-ref theo linCmt() with fixed components", {
+
+      one.cmt <- function() {
+        ini({
+          ## You may label each parameter with a comment
+          tka <- exp(0.45) # Ka
+          tcl <- log(c(0, 2.7, 100)) # Log Cl
+          ## This works with interactive models
+          ## You may also label the preceding line with label("label text")
+          tv <- fix(3.45); label("log V")
+          ## the label("Label name") works with all models
+          eta.ka ~ 0.6
+          eta.cl ~ fix(0.3)
+          eta.v ~ 0.1
+          add.sd <- 0.7
+        })
+        model({
+          ka <- tka * exp(eta.ka)
+          cl <- exp(tcl + eta.cl)
+          v <- exp(tv + eta.v)
+          linCmt() ~ add(add.sd)
+        })
+      }
+
+    f <- nlmixr(one.cmt)
+
+    expect_equal(f$saemModel0,
+                 quote(rxModelVars({
+                   ka <- tka * exp(eta.ka)
+                   cl <- exp(tcl)
+                   v <- exp(tv)
+                   rx_pred_ <- linCmt()
+                 })))
+
+    expect_equal(f$saemParamsToEstimate,
+                 c("tka", "tcl", "tv", "eta.ka"))
+
+    expect_equal(f$saemParams,
+                 "params(tka,tcl,tv,eta.ka)")
+
+    expect_equal(f$saemInParsAndMuRefCovariates,
+                 list(inPars = character(0), covars = character(0)))
+
+    expect_equal(f$saemFixed,
+                 c(tka = FALSE, tcl = FALSE, tv = TRUE, eta.ka=TRUE))
+
+    expect_equal(f$saemEtaTrans,
+                 c(4L, 2L, 3L))
+
+    expect_equal(f$saemOmegaTrans,
+                 c(3L, 1L, 2L))
+
+    expect_equal(f$saemModelOmega,
+                 structure(c(0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1), .Dim = c(4L, 4L)))
+
+    expect_equal(f$saemModelOmegaFixed,
+                 structure(c(0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), .Dim = c(4L, 4L)))
+
+    expect_equal(f$saemModelOmegaFixedValues,
+                 structure(c(0.6, 0, 0, 0, 0.3, 0, 0, 0, 0.1), .Dim = c(3L, 3L)))
+e
+    expect_equal(f$saemLow, -Inf)
+
+    expect_equal(f$saemHi, -Inf)
+
+    expect_equal(f$saemPropT, 0) # not proportional on transformed scale
+
+    expect_equal(f$saemYj, 2) # normal translation
+
+    expect_equal(f$saemResMod, 1) # additive
+
+    # Don't keep the eta.cl since it is fixed
+    expect_equal(f$saemParHistOmegaKeep,
+                 c(eta.cl = 0L, eta.v = 1L, eta.ka = 1L))
+
+    # fixed volume and fixed eta.cl
+    expect_equal(f$saemParHistNames,
+                 c("tka", "tcl", "V(eta.v)", "V(eta.ka)", "add.sd"))
+
+    # y= f + (a + b*f^c)*varepsilon # combined1
+    # y= f + sqrt(a^2 + b^2*f^(2*c))*varepsilon # combined2
+
+    # Ares
+    expect_equal(f$saemAres, 0.7)
+
+    # Bres
+    expect_equal(f$saemBres, 1)
+
+    # Cres
+    expect_equal(f$saemCres, 1)
+
+    # lambda for boxCox or yeoJohnson
+    expect_equal(f$saemLres, 1)
+
+    expect_equal(f$saemLogEta, c(tka = FALSE, tcl = TRUE, tv = TRUE, eta.ka=TRUE))
+
+    expect_equal(f$saemInitTheta,
+                 structure(c(1.56831218549017, 2.7, 31.5003923087479), .Names = c("", "", "")))
+
+    expect_equal(f$saemInitOmega,
+                 c(tka = 0.6, tcl = 0.3, tv = 0.1))
+
+    expect_equal(f$saemThetaDataFrame,
+                 structure(list(lower = c(-Inf, -Inf, -Inf, -Inf), theta = c(0.45, 0.993251773010283, 3.45, 0.7), fixed = c(FALSE, FALSE, FALSE, FALSE), upper = c(Inf, Inf, Inf, Inf)), class = "data.frame", row.names = c("tka", "tcl", "tv", "add.sd")))
+
 
   })
 })
