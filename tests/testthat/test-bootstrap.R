af8d279d254368c8594bfc8bb93a1a8aa6a7eb79
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-01-13 17:50:38 -0600

    Fix bootstrap fit and fix tests

--- tests/testthat/test-bootstrap.R
+++ tests/testthat/test-bootstrap.R
@@ -1,17 +1,18 @@
-nlmixrTest(
+nlmixr2Test(
   {
     context("bootstrap")
 
     samp_dat <- theo_sd
 
     test_that("sampling should return different datasets at each call", {
-      a <- digest::digest(nlmixr:::sampling(samp_dat))
-      b <- digest::digest(nlmixr:::sampling(samp_dat))
+      a <- digest::digest(nlmixr2:::sampling(samp_dat))
+      b <- digest::digest(nlmixr2:::sampling(samp_dat))
       testthat::expect_false(isTRUE(all.equal(a, b)))
     })
 
 
     test_that("resuming the fit should not return the same datasets as before", {
+
       one.cmt <- function() {
         ini({
           ## You may label each parameter with a comment
@@ -44,11 +45,11 @@ nlmixrTest(
       ))
 
 
-      fit1 <- nlmixr:::bootstrapFit(fit, nboot = 2, restart = TRUE)
-      fit2 <- nlmixr:::bootstrapFit(fit, nboot = 4, restart = FALSE)
+      fit1 <- nlmixr2:::bootstrapFit(fit, nboot = 2, restart = TRUE)
+      fit2 <- nlmixr2:::bootstrapFit(fit, nboot = 4, restart = FALSE)
 
       output_dir <-
-        paste0("nlmixrBootstrapCache_", "fit", "_", fit$bootstrapMd5)
+        paste0("nlmixr2BootstrapCache_", "fit", "_", fit$bootstrapMd5)
 
       fnameBootDataPattern <- paste0("boot_data",
         "_", "[0-9]+", ".rds",
@@ -74,6 +75,7 @@ nlmixrTest(
     })
 
     testthat::test_that("different confidence levels should result in different bands", {
+
       one.cmt <- function() {
         ini({
           ## You may label each parameter with a comment
@@ -97,7 +99,7 @@ nlmixrTest(
         })
       }
 
-      fit <- suppressWarnings(nlmixr(
+      fit <- suppressWarnings(nlmixr2(
         one.cmt,
         samp_dat,
         est = "focei",
@@ -105,9 +107,9 @@ nlmixrTest(
         table = list(npde = TRUE, cwres = TRUE)
       ))
 
-      fitlist <- nlmixr:::modelBootstrap(fit, nboot = 4, restart = TRUE)[[1]]
-      bootSummary1 <- nlmixr:::getBootstrapSummary(fitlist, ci = 0.95)
-      bootSummary2 <- nlmixr:::getBootstrapSummary(fitlist, ci = 0.75)
+      fitlist <- nlmixr2:::modelBootstrap(fit, nboot = 4, restart = TRUE)[[1]]
+      bootSummary1 <- nlmixr2:::getBootstrapSummary(fitlist, ci = 0.95)
+      bootSummary2 <- nlmixr2:::getBootstrapSummary(fitlist, ci = 0.75)
 
       a <- digest::digest(bootSummary1$parFixedDf$confLower)
       b <- digest::digest(bootSummary2$parFixedDf$confLower)
@@ -120,6 +122,7 @@ nlmixrTest(
     })
 
     testthat::test_that("expected columns in fit$parFixedDf object should match", {
+
       one.cmt <- function() {
         ini({
           ## You may label each parameter with a comment
@@ -143,7 +146,7 @@ nlmixrTest(
         })
       }
 
-      fit <- suppressWarnings(nlmixr(
+      fit <- suppressWarnings(nlmixr2(
         one.cmt,
         samp_dat,
         est = "focei",
@@ -152,16 +155,16 @@ nlmixrTest(
       ))
 
       colsBefore <- colnames(fit$parFixedDf)
-      fitlist <- nlmixr:::modelBootstrap(fit, nboot = 4, restart = TRUE)[[1]]
+      fitlist <- nlmixr2:::modelBootstrap(fit, nboot = 4, restart = TRUE)[[1]]
 
-      bootSummary <- nlmixr:::getBootstrapSummary(fitlist, ci = 0.95)
+      bootSummary <- nlmixr2:::getBootstrapSummary(fitlist, ci = 0.95)
 
       colsAfter <- colnames(fit$parFixedDf)
 
       testthat::expect_equal(colsAfter, colsBefore)
 
       lapply(
-        list.files("./", pattern = "nlmixrBootstrapCache_.*"),
+        list.files("./", pattern = "nlmixr2BootstrapCache_.*"),
         function(x) {
           unlink(x, recursive = TRUE, force = TRUE)
         }
