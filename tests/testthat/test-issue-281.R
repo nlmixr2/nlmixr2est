fb98ef87fdab338f8fe7d078782d015e0eee0cef
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-01-29 22:49:43 -0600

    Start deleting and moving nlmixr issues

--- tests/testthat/test-issue-281.R
+++ tests/testthat/test-issue-281.R
@@ -1,4 +1,5 @@
 test_that("saem building works; Issue nlmixr#281", {
+
   Lesion7 <- function() {
     ini({
       temaxD <- -0.05 ; label("typical value of drug emax")
@@ -9,7 +10,7 @@ test_that("saem building works; Issue nlmixr#281", {
       tslope <- -1 ; label("typical value of growth parameter")
       TRXslope <- -0.1
       tdelay <- 7.15
-      
+
       eta.emaxD ~ 0.3
       eta.emaxT ~ 0.3
       eta.slope ~ 0.3
@@ -22,30 +23,31 @@ test_that("saem building works; Issue nlmixr#281", {
       ec50 <- exp(tec50)
       emaxT <- exp(temaxT + eta.emaxT)
       et50 <- exp(tet50)
-      
+
       slope <- exp(tslope + TRX * TRXslope + eta.slope)
-      
+
       delay <- exp(tdelay + eta.delay)
       kin <- exp(tkin)
       kout <- kin
       GAM <- exp(1) / 2 # Hill coef
       C2 <- centr / V
       CONC <- (C2)^2
-      
+
       Stim1 <- emaxT * (time) / (time + et50)
       Stim2 <- emaxD * (CONC^GAM) / (CONC^GAM + ec50^GAM)
       Stim <- Stim1 * (1 + TRX * Stim2)
-      
+
       Delta <- 1 / (1 + exp(-20 * (time - delay)))
       d / dt(depot) <- -KA * depot
       d / dt(centr) <- KA * depot - CL * C2
       d / dt(Resp) <- kin * (1 + Delta * slope) - kout * (1 + Stim) * Resp
-      
+
       Resp ~ add(add.err)
     })
   }
-  
+
   tmp <- nlmixr(Lesion7)
-  
-  expect_error(tmp$saem.model, NA)
+
+  expect_error(tmp$saemModel, NA)
+  expect_error(tmp$saemModelPred, NA)
 })
