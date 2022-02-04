dbbf842dd48b5c8bee845cc3f451f9451d5fcbce
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-02-03 18:54:32 -0600

    Fix some expectations

--- tests/testthat/test-saem-translate.R
+++ tests/testthat/test-saem-translate.R
@@ -1,4 +1,5 @@
 test_that("Standard theo linCmt()", {
+
   one.cmt <- function() {
     ini({
       tka <- 0.45 ; label("Ka")
@@ -16,9 +17,9 @@ test_that("Standard theo linCmt()", {
       linCmt() ~ add(add.sd)
     })
   }
-  
+
   f <- nlmixr(one.cmt)
-  
+
   expect_equal(f$saemModel0,
                quote(rxModelVars({
                  ka <- exp(tka)
@@ -26,77 +27,76 @@ test_that("Standard theo linCmt()", {
                  v <- exp(tv)
                  rx_pred_ <- linCmt()
                })))
-  
+
   expect_equal(f$saemParamsToEstimate,
                c("tka", "tcl", "tv"))
-  
+
   expect_equal(f$saemParams,
                "params(tka,tcl,tv)")
-  
+
   expect_equal(f$saemInParsAndMuRefCovariates,
                list(inPars = character(0), covars = character(0)))
-  
+
   expect_equal(f$saemFixed,
                c(tka = FALSE, tcl = FALSE, tv = FALSE))
-  
+
   expect_equal(f$saemEtaTrans,
                1:3)
-  
+
   expect_equal(f$saemOmegaTrans,
                1:3)
-  
-  expect_equal(f$saemModelOmega,
-               structure(c(1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), .Dim = c(8L, 8L)))
-  
+
+  expect_equal(f$saemModelOmega, diag(3))
+
   expect_equal(f$saemModelOmegaFixed,
-               matrix(rep(0, 8 * 8), 8, 8))
-  
+               matrix(rep(0, 3 * 3), 3, 3))
+
   expect_equal(f$saemModelOmegaFixedValues,
                structure(c(0.6, 0, 0, 0, 0.3, 0, 0, 0, 0.1), .Dim = c(3L, 3L)))
-  
+
   expect_equal(f$saemLow, -Inf)
-  
-  expect_equal(f$saemHi, -Inf)
-  
+
+  expect_equal(f$saemHi, Inf)
+
   expect_equal(f$saemPropT, 0) # not proportional on transformed scale
-  
+
   expect_equal(f$saemYj, 2) # normal translation
-  
+
   expect_equal(f$saemResMod, 1) # additive
-  
+
   # keep all omegas
   expect_equal(f$saemParHistOmegaKeep,
                c(eta.ka = 1L, eta.cl = 1L, eta.v = 1L))
-  
+
   expect_equal(f$saemParHistNames,
                c("tka", "tcl", "tv", "V(eta.ka)", "V(eta.cl)", "V(eta.v)", "add.sd"))
-  
+
   # y= f + (a + b*f^c)*varepsilon # combined1
   # y= f + sqrt(a^2 + b^2*f^(2*c))*varepsilon # combined2
-  
+
   # Ares
   expect_equal(f$saemAres, 0.7)
-  
+
   # Bres
   expect_equal(f$saemBres, 1)
-  
+
   # Cres
   expect_equal(f$saemCres, 1)
-  
+
   # lambda for boxCox or yeoJohnson
   expect_equal(f$saemLres, 1)
-  
+
   expect_equal(f$saemLogEta, c(tka = TRUE, tcl = TRUE, tv = TRUE))
-  
+
   expect_equal(f$saemInitTheta,
                structure(c(1.56831218549017, 2.7, 31.5003923087479), .Names = c("", "", "")))
-  
+
   expect_equal(f$saemInitOmega,
                c(tka = 0.6, tcl = 0.3, tv = 0.1))
-  
+
   expect_equal(f$saemThetaDataFrame,
                structure(list(lower = c(-Inf, -Inf, -Inf, -Inf), theta = c(0.45, 0.993251773010283, 3.45, 0.7), fixed = c(FALSE, FALSE, FALSE, FALSE), upper = c(Inf, Inf, Inf, Inf)), class = "data.frame", row.names = c("tka", "tcl", "tv", "add.sd")))
-  
+
   expect_equal(f$saemModelPredReplaceLst,
                c(tka = "THETA[1] + ETA[1]", tcl = "THETA[2] + ETA[2]", tv = "THETA[3] + ETA[3]", add.sd = "THETA[4]"))
 })
@@ -119,9 +119,9 @@ test_that("non mu-ref theo linCmt() with fixed components", {
       linCmt() ~ add(add.sd)
     })
   }
-  
+
   f <- nlmixr(one.cmt)
-  
+
   expect_equal(f$saemModel0,
                quote(rxModelVars({
                  ka <- tka * exp(eta.ka)
@@ -129,75 +129,75 @@ test_that("non mu-ref theo linCmt() with fixed components", {
                  v <- exp(tv)
                  rx_pred_ <- linCmt()
                })))
-  
+
   expect_equal(f$saemParamsToEstimate,
                c("tka", "tcl", "tv", "eta.ka"))
-  
+
   expect_equal(f$saemParams,
                "params(tka,tcl,tv,eta.ka)")
-  
+
   expect_equal(f$saemInParsAndMuRefCovariates,
                list(inPars = character(0), covars = character(0)))
-  
+
   expect_equal(f$saemFixed,
                c(tka = FALSE, tcl = FALSE, tv = TRUE, eta.ka=TRUE))
-  
+
   expect_equal(f$saemEtaTrans,
                c(4L, 2L, 3L))
-  
+
   expect_equal(f$saemOmegaTrans,
                c(3L, 1L, 2L))
-  
+
   expect_equal(f$saemModelOmega,
                structure(c(0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1), .Dim = c(4L, 4L)))
-  
+
   expect_equal(f$saemModelOmegaFixed,
                structure(c(0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), .Dim = c(4L, 4L)))
-  
+
   expect_equal(f$saemModelOmegaFixedValues,
                structure(c(0, 0, 0, 0, 0, 0.3, 0, 0, 0, 0, 0.1, 0, 0, 0, 0, 0.6), .Dim = c(4L, 4L)))
-  
+
   expect_equal(f$saemLow, -Inf)
-  
+
   expect_equal(f$saemHi, Inf)
-  
+
   expect_equal(f$saemPropT, 0) # not proportional on transformed scale
-  
+
   expect_equal(f$saemYj, 2) # normal translation
-  
+
   expect_equal(f$saemResMod, 1) # additive
-  
+
   # Don't keep the eta.cl since it is fixed
   expect_equal(f$saemParHistOmegaKeep,
                c(eta.cl = 0L, eta.v = 1L, eta.ka = 1L))
-  
+
   # fixed volume and fixed eta.cl
   expect_equal(f$saemParHistNames,
                c("tka", "tcl", "V(eta.v)", "V(eta.ka)", "add.sd"))
-  
+
   # y= f + (a + b*f^c)*varepsilon # combined1
   # y= f + sqrt(a^2 + b^2*f^(2*c))*varepsilon # combined2
-  
+
   # Ares
   expect_equal(f$saemAres, 0.7)
-  
+
   # Bres
   expect_equal(f$saemBres, 1)
-  
+
   # Cres
   expect_equal(f$saemCres, 1)
-  
+
   # lambda for boxCox or yeoJohnson
   expect_equal(f$saemLres, 1)
-  
+
   expect_equal(f$saemLogEta, c(tka = FALSE, tcl = TRUE, tv = TRUE, eta.ka=TRUE))
-  
+
   expect_equal(f$saemInitTheta,
                c(1.56831218549017, 2.7, FIXED=31.5003923087479, FIXED=1))
-  
+
   expect_equal(f$saemInitOmega,
                c(tka = 1.0, tcl = 0.3, tv = 0.1, eta.ka=0.6))
-  
+
   expect_equal(f$saemThetaDataFrame,
                structure(list(lower = c(-Inf, -Inf, -Inf, -Inf),
                               theta = c(exp(0.45), 0.993251773010283, 3.45, 0.7),
@@ -205,7 +205,7 @@ test_that("non mu-ref theo linCmt() with fixed components", {
                               upper = c(Inf, Inf, Inf, Inf)),
                          class = "data.frame",
                          row.names = c("tka", "tcl", "tv", "add.sd")))
-  
+
   expect_equal(f$saemModelPredReplaceLst,
                c(tka = "THETA[1]", tcl = "THETA[2] + ETA[2]", tv = "THETA[3] + ETA[3]", eta.ka = "ETA[1]", add.sd = "THETA[4]"))
 })
@@ -229,15 +229,15 @@ test_that("theo wt cov parsing", {
       linCmt() ~ add(add.sd)
     })
   }
-  
+
   f <- one.cmt()
-  
+
   expect_equal(f$saemModelPredReplaceLst,
                c(tka = "THETA[1] + ETA[1]", tcl = "THETA[2] + ETA[2] + wt * THETA[4]", tv = "THETA[3] + ETA[3]", cl.wt = "THETA[4]", add.sd = "THETA[5]"))
 })
 
 test_that("nimo parsing", {
-  
+
   nimo <- function() {
     ini({
       tcl <- log(0.001)
@@ -262,28 +262,28 @@ test_that("nimo parsing", {
       kint <- exp(tkint)
       ksyn <- exp(tksyn)
       kdeg <- exp(tkdeg)
-      
+
       k <- cl/v1
       k12 <- Q/v1
       k21 <- Q/v2
-      
+
       eff(0) <- ksyn/kdeg ##initializing compartment
-      
+
       ## Concentration is calculated
       conc = 0.5*(central/v1-eff-kss)+0.5*sqrt((central/v1-eff-kss)**2+4*kss*central/v1)
-      
+
       d/dt(central)  = -(k+k12)*conc*v1+k21*peripheral-kint*eff*conc*v1/(kss+conc)
       d/dt(peripheral) = k12*conc*v1-k21*peripheral  ##Free Drug second compartment amount
       d/dt(eff) = ksyn - kdeg*eff - (kint-kdeg)*conc*eff/(kss+conc)
-      
+
       IPRED=log(conc)
-      
+
       IPRED ~ add(add.err)
     })
   }
-  
+
   f <- nlmixr(nimo)
-  
+
   expect_equal(
     f$saemModel0,
     quote(rxModelVars({
@@ -308,89 +308,89 @@ test_that("nimo parsing", {
       IPRED = log(conc)
       rx_pred_ <- IPRED
     })))
-  
+
   expect_equal(
     f$saemParamsToEstimate,
     c("tcl", "tv1", "tQ", "tv2", "tkss", "tkint", "tksyn", "tkdeg")
   )
-  
+
   expect_equal(
     f$saemParams,
     "params(tcl,tv1,tQ,tv2,tkss,tkint,tksyn,tkdeg)"
   )
-  
+
   expect_equal(
     f$saemInParsAndMuRefCovariates,
     list(inPars = character(0), covars = character(0))
   )
-  
+
   expect_equal(
     f$saemFixed,
     c(tcl = FALSE, tv1 = FALSE, tQ = FALSE, tv2 = FALSE, tkss = FALSE, tkint = FALSE, tksyn = FALSE, tkdeg = FALSE)
   )
-  
+
   # eta.cl, eta.v1, eta.kss
   expect_equal(
     f$saemEtaTrans,
     c(1L, 2L, 5L)
   )
-  
+
   expect_equal(f$saemOmegaTrans, 1:3)
-  
+
   expect_equal(
     f$saemModelOmegaFixedValues,
     structure(c(2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), .Dim = c(8L, 8L))
   )
 
   expect_equal(f$saemLow, -Inf)
-  
+
   expect_equal(f$saemHi, Inf)
-  
+
   expect_equal(f$saemPropT, 0) # not proportional on transformed scale
-  
+
   expect_equal(f$saemYj, 2) # normal translation
-  
+
   expect_equal(f$saemResMod, 1) # additive
-  
+
   # Don't keep the eta.cl since it is fixed
   expect_equal(f$saemParHistOmegaKeep,
                c(eta.cl = 1L, eta.v1 = 1L, eta.kss = 1L))
-  
+
   # fixed volume and fixed eta.cl
   expect_equal(f$saemParHistNames,
                c("tcl", "tv1", "tQ", "tv2", "tkss", "tkint", "tksyn", "tkdeg",
                  "V(eta.cl)", "V(eta.v1)", "V(eta.kss)", "add.err"))
-  
+
   # y= f + (a + b*f^c)*varepsilon # combined1
   # y= f + sqrt(a^2 + b^2*f^(2*c))*varepsilon # combined2
-  
+
   # Ares
   expect_equal(f$saemAres, 10)
-  
+
   # Bres
   expect_equal(f$saemBres, 1)
-  
+
   # Cres
   expect_equal(f$saemCres, 1)
-  
+
   # lambda for boxCox or yeoJohnson
   expect_equal(f$saemLres, 1)
-  
+
   expect_equal(
     f$saemLogEta,
     c(tcl = TRUE, tv1 = TRUE, tQ = TRUE, tv2 = TRUE, tkss = TRUE, tkint = TRUE, tksyn = TRUE, tkdeg = TRUE)
   )
-  
+
   expect_equal(
     f$saemInitTheta,
     structure(c(0.001, 1.45, 0.004, 44, 12, 0.3, 1, 7), .Names = c("", "", "", "", "", "", "", ""))
   )
-  
+
   expect_equal(
     f$saemInitOmega,
     c(tcl = 2, tv1 = 2, tQ = 1, tv2 = 1, tkss = 2, tkint = 1, tksyn = 1, tkdeg = 1)
   )
-  
+
   expect_equal(
     f$saemThetaDataFrame,
     structure(
