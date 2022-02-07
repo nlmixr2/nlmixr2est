e09524f0f829157c6c83a3cae37824110338dc09
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-02-06 22:38:56 -0600

    Add test for all the timing that is exposed

--- tests/testthat/test-timing.R
+++ tests/testthat/test-timing.R
@@ -1,6 +1,5 @@
-nm <- loadNamespace("nlmixr2")
 
-test_that("test nesting of the timing engine", {
+test_that("test timing engine", {
 
   env <- new.env(parent=emptyenv())
   env$time <- data.frame(t0=1)
@@ -10,10 +9,22 @@ test_that("test nesting of the timing engine", {
     # note this can be nested, time1 will exclude the timing from time2
     nlmixrWithTiming("time2", {
       Sys.sleep(0.25)
+      nlmixrWithTiming("t0", {
+        Sys.sleep(0.1)
+      }, envir=env)
     }, envir=env)
   }, envir=env)
 
   expect_true(env$time$time2 < 0.4)
   expect_true(env$time$time1 < 0.4)
+  expect_true(env$time$t0 > 1)
+
+  nlmixrAddTiming(env, "time2", 3)
+
+  expect_true(env$time$time2 > 3)
+
+  nlmixrAddTiming(env, "new", 3.5)
+
+  expect_true(env$time$new > 3)
 
 })
