45b4fafcc6e48b0acaede100c9d4999746ef0dc8
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-02-12 10:36:22 -0600

    Add more or less complete nlme

--- tests/testthat/test-nlme.R
+++ tests/testthat/test-nlme.R
@@ -1,113 +1,206 @@
-model.2cpt.ode.wtcl.sexv2 <- function() {
-  ini({
-    tka <- log(1.14)
-    tcl <- log(0.0190)
-    tv2  <- log(2.12)
-    tv3  <- log(20.4)
-    tq   <- log(0.383)
-    wteff  <- 0.35
-    sexeff <- -0.2
-    eta.ka ~ 0.1
-    eta.cl ~ 0.2
-    eta.v2 ~ 0.3
-    eta.v3 ~ 0.4
-    eta.q ~ 0.5
-    prop.err <- 0.075
-  })
-  model({
-    ka <- exp(tka + eta.ka)
-    cl <- exp(tcl + wteff*log(WT/70) + eta.cl)
-    v2 <- exp(tv2 + sexeff*(SEX) + eta.v2)
-    v3 <- exp(tv3 + eta.v3)
-    q  <- exp(tq + eta.q)
-    d/dt(depot) = -ka * depot
-    d/dt(center) = ka * depot - cl / v2 * center + q/v3 * periph - q/v2 * center
-    d/dt(periph) = q/v2 * center - q/v3 * periph
-    cp = center / v2
-    cp ~ prop(prop.err)
-  })
-}
+test_that("nlme one compartment theo_sd", {
 
-f <- nlmixr(model.2cpt.ode.wtcl.sexv2)
+  one.compartment <- function() {
+    ini({
+      tka <- 0.45 # Log Ka
+      tcl <- 1 # Log Cl
+      tv <- 3.45    # Log V
+      eta.ka ~ 0.6
+      eta.cl ~ 0.3
+      eta.v ~ 0.1
+      add.sd <- 0.7
+    })
+    model({
+      ka <- exp(tka + eta.ka)
+      cl <- exp(tcl + eta.cl)
+      v <- exp(tv + eta.v)
+      d/dt(depot) = -ka * depot
+      d/dt(center) = ka * depot - cl / v * center
+      cp = center / v
+      cp ~ add(add.sd)
+    })
+  }
 
- model.2cpt.ode.wtcl.sexv2 <- function() {
-  ini({
-    tka <- log(1.14)
-    tcl <- log(0.0190)
-    tv2  <- log(2.12)
-    tv3  <- log(20.4)
-    tq   <- log(0.383)
-    wteff  <- 0.35
-    sexeff <- -0.2
-    eta.ka + eta.cl ~ c(0.1,
-                        0.001, 0.2)
-    eta.v2 ~ 0.3
-    eta.v3 ~ 0.4
-    eta.q ~ 0.5
-    prop.err <- 0.075
-  })
-  model({
-    ka <- exp(tka + eta.ka)
-    cl <- exp(tcl + wteff*log(WT/70) + eta.cl)
-    v2 <- exp(tv2 + sexeff*(SEX) + eta.v2)
-    v3 <- exp(tv3 + eta.v3)
-    q  <- exp(tq + eta.q)
-    d/dt(depot) = -ka * depot
-    d/dt(center) = ka * depot - cl / v2 * center + q/v3 * periph - q/v2 * center
-    d/dt(periph) = q/v2 * center - q/v3 * periph
-    cp = center / v2
-    cp ~ prop(prop.err)
-  })
-}
+  f <- one.compartment()
 
-f <- nlmixr(model.2cpt.ode.wtcl.sexv2)
+  nlme  <- nlmixr(f, theo_sd, "nlme", control=nlmeControl( ))
+  expect_true(inherits(nlme, "nlmixr2FitData"))
+
+  one.compartment <- function() {
+    ini({
+      tka <- 0.45 # Log Ka
+      tcl <- 1 # Log Cl
+      tv <- 3.45    # Log V
+      eta.ka ~ 0.6
+      eta.cl + eta.v ~ c(0.3,
+                         0.001, 0.1)
+      add.sd <- 0.7
+    })
+    model({
+      ka <- exp(tka + eta.ka)
+      cl <- exp(tcl + eta.cl)
+      v <- exp(tv + eta.v)
+      d/dt(depot) = -ka * depot
+      d/dt(center) = ka * depot - cl / v * center
+      cp = center / v
+      cp ~ add(add.sd)
+    })
+  }
+
+  f <- one.compartment()
+
+  nlme  <- nlmixr(f, theo_sd, "nlme", control=nlmeControl(maxIter=5))
+
+  expect_true(inherits(nlme, "nlmixr2FitData"))
+
+  one.compartment <- function() {
+    ini({
+      tka <- exp(0.45) # Log Ka
+      tcl <- 1 # Log Cl
+      tv <- 3.45    # Log V
+      eta.ka ~ 0.6
+      eta.cl ~ 0.3
+      eta.v ~ 0.1
+      add.sd <- 0.7
+    })
+    model({
+      ka <- tka * exp(eta.ka)
+      cl <- exp(tcl + eta.cl)
+      v <- exp(tv + eta.v)
+      d/dt(depot) = -ka * depot
+      d/dt(center) = ka * depot - cl / v * center
+      cp = center / v
+      cp ~ add(add.sd)
+    })
+  }
+
+  f <- one.compartment()
+
+  nlme  <- nlmixr(f, theo_sd, "nlme", control=nlmeControl(maxIter=2))
+  expect_true(inherits(nlme, "nlmixr2FitData"))
+
+  expect_error(nlmixr(f, theo_sd, "nlme", control=nlmeControl(maxIter=2, sens=TRUE)))
+
+})
 
 
+test_that("Other error structures", {
 
+  dat <- Wang2007
+  dat$DV <- dat$Y
 
-model.2cpt.cf.wtcl.sexv2 <- function() {
-  ini({
-    tka <- log(1.5)
-    tcl <- log(0.05)
-    tv2  <- log(2.5)
-    tv3  <- log(25)
-    tq   <- log(0.5)
-    wteff <- 0.01
-    sexeff <- -0.01
-    eta.ka ~ 1
-    eta.cl ~ 1
-    eta.v2 ~ 1
-    eta.v3 ~ 1
-    eta.q ~ 1
-    prop.err <- 0.5
-  })
-  model({
-    ka <- exp(tka + eta.ka)
-    cl <- exp(tcl + wteff*lwt70 + eta.cl)
-    v2 <- exp(tv2 + sexeff*SEX + eta.v2)
-    v3 <- exp(tv3 + eta.v3)
-    q  <- exp(tq + eta.q)
-    linCmt() ~ prop(prop.err)
-  })
-}
+  mod <- function() {
+    ini({
+      tke <- 0.5
+      eta.ke ~ 0.04
+      prop.sd <- sqrt(0.1)
+    })
+    model({
+      ke <- tke * exp(eta.ke)
+      ipre <- 10 * exp(-ke * t)
+      f2 <- ipre / (ipre + 5)
+      f3 <- f2 * 3
+      lipre <- log(ipre)
+      ipre ~ prop(prop.sd)
+    })
+  }
 
-f <- nlmixr(model.2cpt.cf.wtcl.sexv2)
-## > f$nlme.specs
-## $fixed
-## tka + tcl + tv2 + tv3 + tq + wteff + sexeff ~ 1
-## <environment: 0x5642b95d2638>
+  f <- mod()
 
-## $random
-## Positive definite matrix structure of class pdDiag representing
-##      [,1] [,2] [,3] [,4] [,5]
-## [1,]    1    0    0    0    0
-## [2,]    0    1    0    0    0
-## [3,]    0    0    1    0    0
-## [4,]    0    0    0    1    0
-## [5,]    0    0    0    0    1
+  nlme  <- nlmixr(f, dat, "nlme", control=nlmeControl())
 
-## $start
-##        tka        tcl        tv2        tv3         tq      wteff     sexeff
-##  0.4054651 -2.9957323  0.9162907  3.2188758 -0.6931472  0.0100000 -0.0100000
+  expect_true(inherits(nlme, "nlmixr2FitData"))
 
-## > `
+  mod <- function() {
+    ini({
+      tke <- 0.5
+      eta.ke ~ 0.04
+      prop.sd <- sqrt(0.1)
+      pw <- 4
+    })
+    model({
+      ke <- tke * exp(eta.ke)
+      ipre <- 10 * exp(-ke * t)
+      f2 <- ipre / (ipre + 5)
+      f3 <- f2 * 3
+      lipre <- log(ipre)
+      ipre ~ pow(prop.sd, pw)
+    })
+  }
+
+  f <- mod()
+
+  nlme  <- nlmixr(mod, dat, "nlme", control=nlmeControl())
+
+  expect_true(inherits(nlme, "nlmixr2FitData"))
+
+  mod <- function() {
+    ini({
+      tke <- 0.5
+      eta.ke ~ 0.04
+      add.sd <- sqrt(0.1)
+      prop.sd <- sqrt(0.1)
+    })
+    model({
+      ke <- tke * exp(eta.ke)
+      ipre <- 10 * exp(-ke * t)
+      f2 <- ipre / (ipre + 5)
+      f3 <- f2 * 3
+      lipre <- log(ipre)
+      ipre ~ add(add.sd) + prop(prop.sd) + combined2()
+    })
+  }
+
+  f <- mod()
+
+  nlme  <- nlmixr(mod, dat, "nlme", control=nlmeControl(msMaxIter=10000))
+
+  expect_true(inherits(nlme, "nlmixr2FitData"))
+
+  mod <- function() {
+    ini({
+      tke <- 0.5
+      eta.ke ~ 0.04
+      add.sd <- sqrt(0.1)
+      prop.sd <- sqrt(0.1)
+    })
+    model({
+      ke <- tke * exp(eta.ke)
+      ipre <- 10 * exp(-ke * t)
+      f2 <- ipre / (ipre + 5)
+      f3 <- f2 * 3
+      lipre <- log(ipre)
+      ipre ~ add(add.sd) + prop(prop.sd) + combined1()
+    })
+  }
+
+  f <- mod()
+
+  nlme  <- nlmixr(mod, dat, "nlme", control=nlmeControl(msMaxIter=10000))
+
+  expect_true(inherits(nlme, "nlmixr2FitData"))
+
+  mod <- function() {
+    ini({
+      tke <- 0.5
+      eta.ke ~ 0.04
+      add.sd <- sqrt(0.1)
+      prop.sd <- sqrt(0.1)
+      pw <- 1
+    })
+    model({
+      ke <- tke * exp(eta.ke)
+      ipre <- 10 * exp(-ke * t)
+      f2 <- ipre / (ipre + 5)
+      f3 <- f2 * 3
+      lipre <- log(ipre)
+      ipre ~ add(add.sd) + pow(prop.sd, pw) + combined1()
+    })
+  }
+
+  f <- mod()
+
+  nlme  <- nlmixr(mod, dat, "nlme", control=nlmeControl(msMaxIter=10000))
+
+})
+
