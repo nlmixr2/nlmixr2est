0f8811af70c3e27f1641ea894cc5232e2c0ecbd2
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-02-19 17:11:21 -0600

    Make sure the rxControl, ci and sigdigTable give the right results

--- tests/testthat/test-control.R
+++ tests/testthat/test-control.R
@@ -1,7 +1,10 @@
 nlmixrControlTest <- function(control) {
+  # Solving options for table
   expect_true(inherits(control$rxControl, "rxControl"))
+  # Options needed for parameter table generation
   expect_true(checkmate::testNumeric(control$ci, lower=0, upper=1, any.missing=FALSE, len=1))
   expect_true(checkmate::testIntegerish(control$sigdigTable, lower=1, any.missing=FALSE, len=1))
+  expect_true(checkmate::testLogical(control$genRxControl, any.missing=FALSE, len=1))
 }
 
 test_that("test foceiControl option sanity", {
@@ -22,6 +25,12 @@ test_that("test foceiControl option sanity", {
   expect_equal(.ctl, .ctl2)
   # resetThetaFinalP
   .ctl <- foceiControl(resetThetaFinalP=0.5)
+  .ctl2 <- do.call(foceiControl, .ctl)
+  expect_equal(.ctl, .ctl2)
+  expect_true(.ctl$genRxControl)
+
+  .ctl <- foceiControl(rxControl=rxControl(sigdig=6))
+  expect_false(.ctl$genRxControl)
   .ctl2 <- do.call(foceiControl, .ctl)
   expect_equal(.ctl, .ctl2)
 
