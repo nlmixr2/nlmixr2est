38fc8595219b37afb5fad07ebf5be66bf5da5ced
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-02-21 22:53:45 -0600

    Fix test to use nlmixr2est

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,7 +3,7 @@ library(rxode2)
 library(nlmixr2est)
 verbose_minimization <- FALSE
 
-test_check("nlmixr2", stop_on_failure = FALSE, wrap=TRUE,
+test_check("nlmixr2est", stop_on_failure = FALSE, wrap=TRUE,
            reporter = testthat::LocationReporter)
 
 
