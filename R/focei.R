07525053ca785e0a28103b08c9d3c6bc3c50e6ee
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-02-23 15:34:53 -0600

    Export nlmixr2CreateOutputFromUi

--- R/focei.R
+++ R/focei.R
@@ -1535,7 +1535,41 @@ nlmixr2Est.output <- function(env, ...) {
   .foceiFamilyReturn(env, .ui, ..., est=env$est)
 }
 
-
+#' Create nlmixr output from the UI
+#'
+#'
+#' @param ui This is the UI that will be used for the translation
+#' @param data This has the data
+#' @param control focei control for data creation
+#' @param table Table options
+#' @param env Environment setup which needs the following:
+#' - `$table` for table options
+#' - `$origData` -- Original Data
+#' - `$dataSav` -- Processed data from .foceiPreProcessData
+#' - `$idLvl` -- Level information for ID factor added
+#' - `$ui` for ui object
+#' - `$fullTheta` Full theta information
+#' - `$etaObf` data frame with ID, etas and OBJI
+#' - `$cov` For covariance
+#' - `$covMethod` for the method of calculating the covariance
+#' - `$adjOf` Should the objective function value be adjusted
+#' - `$objective` objective function value
+#' - `$extra` Extra print information
+#' - `$method` Estimation method (for printing)
+#' - `$omega` Omega matrix
+#' - `$etaObf` Eta objective function data frame
+#' - `$theta` Is a theta data frame
+#' - `$model` a list of model information for table generation.  Needs a `predOnly` model
+#' - `$message` Message for display
+#' - `$est` estimation method
+#' - `$ofvType` (optional) tells the type of ofv is currently being use
+#'
+#' There are some more details that need to be described here
+#'
+#' @param est Estimation method
+#' @return nlmixr fit object
+#' @author Matthew L. Fidler
+#' @export
 nlmixr2CreateOutputFromUi <- function(ui, data=NULL, control=NULL, table=NULL, env=NULL, est="none") {
   if (inherits(ui, "function")) {
     ui <- rxode2::rxode2(ui)
