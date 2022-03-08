aab09d4e00c593535b504fd992cfcd37ec63d668
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-03-07 22:55:45 -0600

    Put focei in there as well

--- tests/testthat/test-tad-in-models.R
+++ tests/testthat/test-tad-in-models.R
@@ -1,4 +1,4 @@
-
+skip_on_cran()
 ## define model
 my.model.pk <- function(){
   ini({
@@ -21,7 +21,7 @@ my.model.pk <- function(){
     n   <- exp(ln + eta.ln)
     # mtt <- (n+1)/ktr
     bio <- 1
-    d/dt(depot)   <-  exp(log(bio*podo)+log(ktr)+n*log(ktr*tad(depot))-ktr*tad(depot)-lgamma(n+1)) - ka * depot
+    d/dt(depot)   <-  exp(log(bio*podo(depot))+log(ktr)+n*log(ktr*tad(depot))-ktr*tad(depot)-lgamma(n+1)) - ka * depot
     d/dt(central) <-  ka * depot - CL/V * central
     central_conc <- central / V
     central_conc ~ add(err.pk)
@@ -29,5 +29,10 @@ my.model.pk <- function(){
 }
 
 ## fit the model
-tmp <- expect_error(nlmixr(my.model.pk, theo_md, est="saem",
-                           control = saemControl(print=0)), NA)
+d <- theo_md %>%
+  dplyr::mutate(EVID=ifelse(EVID == 0, 0L, 7L))
+
+tmp <- expect_error(nlmixr(my.model.pk, theo_md, est="focei", control=foceiControl(print=0L)), NA)
+
+## Now try with saem
+tmp <- expect_error(nlmixr(my.model.pk, theo_md, est="saem", control=saemControl(print=0L)), NA)
