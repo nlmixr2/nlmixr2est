be532d15eb16b473bcba642e9f97e9337336244c
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-05-06 20:46:40 -0500

    Allow nmTest() to specify skip on CRAN just like rxode2

--- R/validate.R
+++ R/validate.R
@@ -3,11 +3,21 @@
 ##' This allows easy validation/qualification of nlmixr2 by running the
 ##' testing suite on your system.
 ##' @param type of test to be run
-##' @param check Use devtools::check to run checks
+##' @param skipOnCran when `TRUE` skip the test on CRAN.
 ##' @author Matthew L. Fidler
 ##' @return Nothing, called for its side effects
 ##' @export
-nlmixr2Validate <- function(type = NULL, check = FALSE) {
+nlmixr2Validate <- function(type = NULL, skipOnCran=TRUE) {
+  if (is(substitute(type), "{")) {
+    if (isTRUE(skipOnCran)) {
+      if (!identical(Sys.getenv("NOT_CRAN"), "true") ||
+            !identical(Sys.getenv("nmTest"), "")) {
+        return(invisible())
+      }
+    }
+    return(force(type))
+  }
+
   pt <- proc.time()
   .filter <- NULL
   if (is.null(type)) type <- FALSE
@@ -17,8 +27,16 @@ nlmixr2Validate <- function(type = NULL, check = FALSE) {
   }
   if (type == TRUE) {
     .oldCran <- Sys.getenv("NOT_CRAN")
-    Sys.setenv("NOT_CRAN"="true")
-    on.exit(Sys.setenv("NOT_CRAN"=.oldCran))
+    .oldNmTest <- Sys.getenv("nmTest")
+    Sys.setenv("NOT_CRAN" = "true") # nolint
+    Sys.setenv("nmTest" = "") # nolint
+    on.exit(Sys.setenv("NOT_CRAN" = .oldCran, "nmTest"=.oldNmTest)) # nolint
+  } else if (type == FALSE) {
+    .oldCran <- Sys.getenv("NOT_CRAN")
+    .oldNmTest <- Sys.getenv("nmTest")
+    Sys.setenv("NOT_CRAN" = "false") # nolint
+    Sys.setenv("rxTest" = "false") # nolint
+    on.exit(Sys.setenv("NOT_CRAN" = .oldCran, "rxTest"=.oldNmTest)) # nolint
   }
   rxode2::.rxWithOptions(list(testthat.progress.max_fails=10000000000), {
     path <- file.path(system.file("tests", package = "nlmixr2est"), "testthat")
