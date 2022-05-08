e3b281e5c02ed006ee99cc615c8f313e5275c3b8
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-05-07 23:19:59 -0500

    Seems to be a bit fragile, skip on CRAN

--- tests/testthat/test-rx-model-props.R
+++ tests/testthat/test-rx-model-props.R
@@ -1,33 +1,37 @@
-test_that("rxode2 model properties work with fits", {
+nmTest({
 
-  one.cmt <- function() {
-    ini({
-      ## You may label each parameter with a comment
-      tka <- 0.45 # Ka
-      tcl <- log(c(0, 2.7, 100)) # Log Cl
-      ## This works with interactive models
-      ## You may also label the preceding line with label("label text")
-      tv <- 3.45; label("log V")
-      ## the label("Label name") works with all models
-      eta.ka ~ 0.6
-      eta.cl ~ 0.3
-      eta.v ~ 0.1
-      add.sd <- 0.7
-    })
-    model({
-      ka <- exp(tka + eta.ka)
-      cl <- exp(tcl + eta.cl)
-      v <- exp(tv + eta.v)
-      linCmt() ~ add(add.sd)
-    })
-  }
+  test_that("rxode2 model properties work with fits", {
 
-  f <- nlmixr(one.cmt)
+    one.cmt <- function() {
+      ini({
+        ## You may label each parameter with a comment
+        tka <- 0.45 # Ka
+        tcl <- log(c(0, 2.7, 100)) # Log Cl
+        ## This works with interactive models
+        ## You may also label the preceding line with label("label text")
+        tv <- 3.45; label("log V")
+        ## the label("Label name") works with all models
+        eta.ka ~ 0.6
+        eta.cl ~ 0.3
+        eta.v ~ 0.1
+        add.sd <- 0.7
+      })
+      model({
+        ka <- exp(tka + eta.ka)
+        cl <- exp(tcl + eta.cl)
+        v <- exp(tv + eta.v)
+        linCmt() ~ add(add.sd)
+      })
+    }
 
-  fit <- nlmixr(one.cmt, theo_sd, est="saem")
+    f <- nlmixr(one.cmt)
 
-  expect_equal(rxModelVars(one.cmt), rxModelVars(f))
+    fit <- nlmixr(one.cmt, theo_sd, est="saem")
 
-  expect_equal(rxModelVars(f), rxModelVars(fit))
+    expect_equal(rxModelVars(one.cmt), rxModelVars(f))
+
+    expect_equal(rxModelVars(f), rxModelVars(fit))
+
+  })
 
 })
