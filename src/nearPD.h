85fd527a0cf9039c43244901500b3f9f91d2ea26
matt <matthew.fidler@gmail.com>
2022-07-28 15:44:05 -0500

    keep diag is true in nlmixr2est inner

--- src/nearPD.h
+++ src/nearPD.h
@@ -2,7 +2,7 @@
 #define __NEARPD_H__
 #if defined(__cplusplus)
 
-bool nmNearPD(mat &ret, mat x, bool keepDiag = false,
+bool nmNearPD(mat &ret, mat x, bool keepDiag = true,
              bool do2eigen = true, bool doDykstra = true, bool only_values = false,
              double eig_tol   = 1e-6, double conv_tol  = 1e-7, double posd_tol  = 1e-8,
              int maxit    = 100, bool trace = false // set to TRUE (or 1 ..) to trace iterations
