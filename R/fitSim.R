4b1f218d67a7f8fdb36747b09f05f0bd4416a0f6
Matthew Fidler <matthew.fidler@gmail.com>
2023-01-11 23:00:57 -0600

    Add estimation specific methd for $simulationModel; default to $ui

--- R/fitSim.R
+++ R/fitSim.R
@@ -1,9 +1,25 @@
-getBaseSimModel.nlmixr2.focei <- function(obj) {
+#' Method for getting simulation rxode2 classic models based on fits
+#'
+#' @param x list where first element is the fit.  The class represents the estimation method.
+#' @return  model for fit$simulationModel
+#' @author Matthew L. Fidler
+#' @export
+#' @keywords internal
+getBaseSimModelFit <- function(x) {
+  UseMethod("getBaseSimModelFit")
+}
+
+#' @rdname getBaseSimModelFit
+#' @export
+getBaseSimModelFit.focei <- function(x) {
+  obj <- x[[1]]
   .base <-   rxode2::rxCombineErrorLines(obj$ui)
   .base[[1]] <- quote(`rxModelVars`)
   .base <- eval(.base)
   # focei models are always pruned
+  .malert("pruning simulation model...")
   .base <- rxode2::rxPrune(.base)
+  .msuccess("done")
   .ctl <- obj$foceiControl
   if (.ctl$sumProd) {
     .malert("stabilizing round off errors in simulation problem...")
@@ -14,17 +30,40 @@ getBaseSimModel.nlmixr2.focei <- function(obj) {
     .base <- rxode2::rxOptExpr(.base, ifelse(.getRxPredLlikOption(),
                                              "simulation llik model",
                                              "simulation model"))
+    .msuccess("done")
   }
   eval(parse(text=paste0("quote(rxode2({\n", .base, "\n}))")))
 }
 
+#' @rdname getBaseSimModelFit
+#' @export
+getBaseSimModelFit.foce <- getBaseSimModelFit.focei
+
+#' @rdname getBaseSimModelFit
+#' @export
+getBaseSimModelFit.fo <- getBaseSimModelFit.focei
+#' @rdname getBaseSimModelFit
+#' @export
+getBaseSimModelFit.foi <- getBaseSimModelFit.focei
+
+#' @rdname getBaseSimModelFit
+#' @export
+getBaseSimModelFit.posthoc <- getBaseSimModelFit.focei
+
+#' @rdname getBaseSimModelFit
+#' @export
+getBaseSimModelFit.default <- function(x) {
+  .obj <- x[[1]]
+  .ui <- .obj$ui
+  getBaseSimModel(.ui)
+}
+
 getBaseSimModel.nlmixr2FitCoreSilent <- function(obj) {
   .est <- obj$est
-  if (.est %in% c("focei", "foce", "foi", "fo", "posthoc")) {
-    if (!exists(".foceiSim", obj)) {
-      assign(".foceiSim", getBaseSimModel.nlmixr2.focei(obj), envir=obj)
-    }
-    return(obj$.foceiSim)
-  }
-  NextMethod()
+  .ret <- list(obj)
+  class(.ret) <- c(.est, "getBaseSimModelFit")
+  return(getBaseSimModelFit(.ret))
 }
+
+getBaseSimModel.nlmixr2FitData <- getBaseSimModel.nlmixr2FitCoreSilent
+getBaseSimModel.nlmixr2FitCore <- getBaseSimModel.nlmixr2FitCoreSilent
