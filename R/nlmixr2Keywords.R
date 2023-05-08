8504e0ceccce0b483440ab29934f4d78044a973c
John Harrold <john.m.harrold@gmail.com>
2023-05-07 17:41:42 -0700

    Adding nlmxir2Keywords. I also updated the rd files of other functions that were out of sync.

---
+++ R/nlmixr2Keywords.R
@@ -0,0 +1,8 @@
+#' A list and description of the fields in the nlmxir2 object
+#'
+#' @format A data frame with 2 columns and 40 or more rows
+#' \describe{
+#' \item{Field}{Name of the field in the nlmixr2 object}
+#' \item{Description}{Description of the information in the field}
+#' }
+"nlmixr2Keywords"
