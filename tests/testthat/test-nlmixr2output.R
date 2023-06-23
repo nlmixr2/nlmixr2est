4b4c44387d141da030e245729dedfb2f6b06f4e9
Bill Denney <wdenney@humanpredictions.com>
2023-06-22 22:39:49 -0400

    Complete testing of .updateParFixedGetEtaRow

--- tests/testthat/test-nlmixr2output.R
+++ tests/testthat/test-nlmixr2output.R
@@ -1,8 +1,9 @@
 test_that(".updateParFixedGetEtaRow returns correct values", {
+  envPrep <- new.env()
   expect_equal(
     .updateParFixedGetEtaRow(
       .eta = "iivemax",
-      .env = new.env(),
+      .env = envPrep,
       .ome = matrix(25, nrow = 1, dimnames = list("iivemax", "iivemax")),
       .omegaFix = c(iivemax = FALSE),
       .muRefCurEval = data.frame(parameter = "iivemax", curEval = "", low = NA_real_, hi = NA_real_),
@@ -10,4 +11,19 @@ test_that(".updateParFixedGetEtaRow returns correct values", {
     ),
     data.frame(ch = "5.00", v = 25)
   )
+  expect_false(envPrep$.cvOnly)
+
+  envPrep <- new.env()
+  expect_equal(
+    .updateParFixedGetEtaRow(
+      .eta = "iivemax",
+      .env = envPrep,
+      .ome = matrix(0.4, nrow = 1, dimnames = list("iivemax", "iivemax")),
+      .omegaFix = c(iivemax = FALSE),
+      .muRefCurEval = data.frame(parameter = "iivemax", curEval = "exp", low = NA_real_, hi = NA_real_),
+      .sigdig = 3L
+    ),
+    data.frame(ch = "70.1", v = sqrt(exp(0.4) - 1) * 100)
+  )
+  expect_false(envPrep$.sdOnly)
 })
