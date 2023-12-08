09d9dfe36ec2b598e8aa58973d310582a5660adb
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-12-08 11:03:56 -0600

    More mem free

--- tests/testthat/test-udf.R
+++ tests/testthat/test-udf.R
@@ -37,7 +37,13 @@ nmTest({
   expect_equal(udfCheck(rxModelVars(.env$f$foceiModel$predOnly)$udf), c("gg"=2L))
   expect_equal(udfCheck(rxModelVars(.env$f$foceiModel$predNoLhs)$udf), c("gg"=2L))
 
+  rxode2::rxUnloadAll()
+  gc()
+
   expect_error({.env$s <- nlmixr2(f, dat, "saem")}, NA)
+
+  rxode2::rxUnloadAll()
+  gc()
 
   expect_error({.env$n <- nlmixr2(f, dat, "nlme")}, NA)
 
@@ -54,17 +60,38 @@ nmTest({
     })
   }
 
+  rxode2::rxUnloadAll()
+  gc()
+
   expect_error({.env$nlm <- nlmixr2(g, dat, "nlm")}, NA)
 
+  rxode2::rxUnloadAll()
+  gc()
+
+
   expect_error({.env$optim <- nlmixr2(g, dat, "optim")}, NA)
+
+  rxode2::rxUnloadAll()
+  gc()
+
 
   ## expect_error({.env$nls <- nlmixr2(g, dat, "nls")}, NA)
 
   expect_error({.env$nlminb <- nlmixr2(g, dat, "nlminb")}, NA)
 
+  rxode2::rxUnloadAll()
+  gc()
+
+
   expect_error({.env$bobyqa <- nlmixr2(g, dat, "bobyqa")}, NA)
 
+  rxode2::rxUnloadAll()
+  gc()
+
   expect_error({.env$lbfgsb3c <- nlmixr2(g, dat, "lbfgsb3c")}, NA)
+
+  rxode2::rxUnloadAll()
+  gc()
 
   expect_error({.env$n1qn1 <- nlmixr2(g, dat, "n1qn1")}, NA)
 
@@ -74,11 +101,24 @@ nmTest({
 
   rxClean()
 
+  rxode2::rxUnloadAll()
+  gc()
+
   .env$f2 <- nlmixr2(f, dat, "focei")
+
+  rxode2::rxUnloadAll()
+  gc()
 
   .env$s2 <- nlmixr2(f, dat, "saem")
 
+  rxode2::rxUnloadAll()
+  gc()
+
   .env$n2 <- nlmixr2(f, dat, "nlme")
+
+  rxode2::rxUnloadAll()
+  gc()
+
 
   rxode2::rxRmFun("gg")
 
