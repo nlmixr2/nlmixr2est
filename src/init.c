c3e5281a2a936d15e5c5011df2afddc1138c255e
Matthew Fidler <matthew.fidler@gmail.com>
2024-09-05 16:07:24 -0500

    Remove poped

--- src/init.c
+++ src/init.c
@@ -113,16 +113,6 @@ SEXP _nlmixr2est_nlmSetScaleC(SEXP);
 SEXP _nlmixr2est_nlmPrintHeader(void);
 SEXP _nlmixr2est_nlmGetParHist(SEXP);
 
-SEXP _nlmixr2est_popedSolveIdN(SEXP, SEXP, SEXP, SEXP);
-SEXP _nlmixr2est_popedSolveIdN2(SEXP, SEXP, SEXP, SEXP);
-SEXP _nlmixr2est_popedSetup(SEXP, SEXP);
-SEXP _nlmixr2est_popedFree(void);
-
-SEXP _nlmixr2est_popedSolveIdME(SEXP, SEXP, SEXP, SEXP, SEXP,
-                                SEXP, SEXP);
-SEXP _nlmixr2est_popedSolveIdME2(SEXP, SEXP, SEXP, SEXP, SEXP,
-                                 SEXP, SEXP);
-
 SEXP _nlmixr2est_iniLotriPtr(SEXP ptr);
 
 SEXP _nlmixr2est_iniRxodePtrs(SEXP ptr);
@@ -137,12 +127,6 @@ static const R_CallMethodDef CallEntries[] = {
   {"_nlmixr2est_iniRxodePtrs", (DL_FUNC) &_nlmixr2est_iniRxodePtrs, 1},
   {"_nlmixr2est_iniLotriPtr", (DL_FUNC) &_nlmixr2est_iniLotriPtr, 1},
   {"_nlmixr2est_uninformativeEta", (DL_FUNC) &_nlmixr2est_uninformativeEta, 1},
-  {"_nlmixr2est_popedSolveIdME2", (DL_FUNC) &_nlmixr2est_popedSolveIdME2, 7},
-  {"_nlmixr2est_popedSolveIdME", (DL_FUNC) &_nlmixr2est_popedSolveIdME, 7},
-  {"_nlmixr2est_popedFree", (DL_FUNC) &_nlmixr2est_popedFree, 0},
-  {"_nlmixr2est_popedSetup", (DL_FUNC) &_nlmixr2est_popedSetup, 2},
-  {"_nlmixr2est_popedSolveIdN", (DL_FUNC) &_nlmixr2est_popedSolveIdN, 4},
-  {"_nlmixr2est_popedSolveIdN2", (DL_FUNC) &_nlmixr2est_popedSolveIdN2, 4},
   {"_nlmixr2est_nlmGetParHist", (DL_FUNC) &_nlmixr2est_nlmGetParHist, 1},
   {"_nlmixr2est_nlmPrintHeader", (DL_FUNC) &_nlmixr2est_nlmPrintHeader, 0},
   {"_nlmixr2est_nlmSetScaleC", (DL_FUNC) &_nlmixr2est_nlmSetScaleC, 1},
