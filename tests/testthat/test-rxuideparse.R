f6cf909230ec267b40007e4da20dc7f81521fb8e
Matthew Fidler <matthew.fidler@gmail.com>
2024-09-11 05:55:47 -0500

    Add some tests here.

--- tests/testthat/test-rxuideparse.R
+++ tests/testthat/test-rxuideparse.R
@@ -1,8 +1,24 @@
 test_that("foceiControl() deparse", {
 
   expect_equal(rxUiDeparse(foceiControl(innerOpt="BFGS", scaleType="norm", normType="std", derivMethod="central", covDerivMethod="forward", covMethod="s",diagXform="identity", addProp= "combined1", rxControl=rxControl(returnType="data.frame")), "ctl"),
+
                quote(ctl <- foceiControl(derivMethod = "central", covDerivMethod = "forward",
                                          covMethod = "s", diagXform = "identity", innerOpt = "BFGS",
-                                         scaleType = "norm", normType = "std", addProp = "combined1")))
+                                         scaleType = "norm", normType = "std", addProp = "combined1",
+                                         rxControl = rxControl(returnType = "data.frame"))))
 
+})
+
+test_that("n1qn1Control()", {
+  expect_equal(rxUiDeparse(n1qn1Control(), "ctl"),
+               quote(ctl <- n1qn1Control()))
+  expect_equal(rxUiDeparse(n1qn1Control(nsim=100), "ctl"),
+               quote(ctl <- n1qn1Control(nsim=100)))
+})
+
+test_that("nlminbControl()", {
+  expect_equal(rxUiDeparse(nlminbControl(), "ctl"),
+               quote(ctl <- nlminbControl()))
+  expect_equal(rxUiDeparse(nlminbControl(eval.max=100), "ctl"),
+               quote(ctl <- nlminbControl(eval.max=100)))
 })
