32ca123f9cbeee1a858195b038b1f0977d089ca2
Matthew Fidler <matthew.fidler@gmail.com>
2025-07-22 07:50:35 -0500

    [no ci] try adding a cache

--- tests/testthat.R
+++ tests/testthat.R
@@ -9,6 +9,8 @@ if (!identical(Sys.getenv("NOT_CRAN"), "true")) {
   setDTthreads(1L)
 }
 
+rxode2::rxCreateCache()
+
 test_check("nlmixr2est")
 ## test_check("nlmixr2est", stop_on_failure = TRUE,
 ##            reporter = testthat::LocationReporter)
