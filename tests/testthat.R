dc740ac7155aadf3e550622a492aa72ef8c581ac
Matthew Fidler <matthew.fidler@gmail.com>
2025-07-22 21:22:55 -0500

    [no ci] Revert "[no ci] more context to the mac m1 error"
    This reverts commit 394834c64efa6e113750106fb7fe2d4e4fca1af3.

--- tests/testthat.R
+++ tests/testthat.R
@@ -11,6 +11,6 @@ if (!identical(Sys.getenv("NOT_CRAN"), "true")) {
 
 rxode2::rxCreateCache()
 
-## test_check("nlmixr2est")
-test_check("nlmixr2est", stop_on_failure = TRUE,
-           reporter = testthat::LocationReporter)
+test_check("nlmixr2est")
+## test_check("nlmixr2est", stop_on_failure = TRUE,
+##            reporter = testthat::LocationReporter)
