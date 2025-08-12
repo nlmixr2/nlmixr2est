16920eba9a2f9caacde8860d0aefc1d644cfc9f8
Matthew Fidler <matthew.fidler@gmail.com>
2025-08-12 16:11:01 -0500

    Add fix/unfixed test

--- tests/testthat/test-iov.R
+++ tests/testthat/test-iov.R
@@ -30,13 +30,55 @@ nmTest({
   fit <- .nlmixr(one.cmt, theo_iov, est="focei",
                  control=foceiControlFast)
   expect_true(inherits(fit, "nlmixr2FitCore"))
+  expect_false( fit$iniDf[fit$iniDf$name == "iov.cl", "fix"])
 
   fit <- .nlmixr(one.cmt, theo_iov, est="foce",
                  control=foceControlFast)
   expect_true(inherits(fit, "nlmixr2FitCore"))
+  expect_false(fit$iniDf[fit$iniDf$name == "iov.cl", "fix"])
 
   fit <- .nlmixr(one.cmt, theo_iov, est="saem",
                  control=saemControlFast)
   expect_true(inherits(fit, "nlmixr2FitCore"))
+  expect_false(fit$iniDf[fit$iniDf$name == "iov.cl", "fix"])
+
+
+  one.cmt <- function() {
+    ini({
+      ## You may label each parameter with a comment
+      tka <- 0.45 # Log Ka
+      tcl <- log(c(0, 2.7, 100)) # Log Cl
+      ## This works with interactive models
+      ## You may also label the preceding line with label("label text")
+      tv <- 3.45; label("log V")
+      ## the label("Label name") works with all models
+      eta.ka ~ 0.6
+      eta.cl ~ 0.3
+      eta.v ~ 0.1
+      iov.cl ~ fix(0.1) | occ
+      add.sd <- 0.7
+    })
+    model({
+      ka <- exp(tka + eta.ka)
+      cl <- exp(tcl + eta.cl + iov.cl)
+      v <- exp(tv + eta.v)
+      linCmt() ~ add(add.sd)
+    })
+  }
+
+  fit <- .nlmixr(one.cmt, theo_iov, est="focei",
+                 control=foceiControlFast)
+  expect_true(inherits(fit, "nlmixr2FitCore"))
+  expect_true( fit$iniDf[fit$iniDf$name == "iov.cl", "fix"])
+
+  fit <- .nlmixr(one.cmt, theo_iov, est="foce",
+                 control=foceControlFast)
+  expect_true(inherits(fit, "nlmixr2FitCore"))
+  expect_true(fit$iniDf[fit$iniDf$name == "iov.cl", "fix"])
+
+  fit <- .nlmixr(one.cmt, theo_iov, est="saem",
+                 control=saemControlFast)
+  expect_true(inherits(fit, "nlmixr2FitCore"))
+  expect_true(fit$iniDf[fit$iniDf$name == "iov.cl", "fix"])
 
 })
