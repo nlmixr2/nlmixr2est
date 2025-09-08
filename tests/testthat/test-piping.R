71a046cf8ef7f20c6effb592b14eb65b0be042b9
Matthew Fidler <matthew.fidler@gmail.com>
2025-09-08 10:55:28 -0500

    Add test for non-mu referenced case

--- tests/testthat/test-piping.R
+++ tests/testthat/test-piping.R
@@ -55,5 +55,34 @@ nmTest({
 
     expect_error(mod %>% rmEta("etaX"), regexp = "'etaX' not in the model")
 
+
+    mod <- function ()  {
+      description <- "One compartment PK model with linear clearance"
+      ini({
+        lka <- 0.45
+        lcl <- 1
+        lvc <- 3.45
+        propSd <- c(0, 0.5)
+        etaKa ~ 0.1
+        etaCl ~ 0.2
+        etaVc ~ 0.3
+      })
+      model({
+        ka <- exp(lka + etaKa)
+        cl <- exp(lcl + etaCl)
+        vc <- lvc*exp(etaVc)
+        Cc <- linCmt()
+        Cc ~ prop(propSd)
+      })
+    }
+
+    mod1 <- mod %>% rmEta(c("etaVc"))
+
+    expect_true(!rxode2::testExists(mod1, "etaVc"))
+    expect_true(rxode2::testExists(mod1, "etaKa"))
+    expect_true(rxode2::testExists(mod1, "etaCl"))
+
+
+
   })
 })
