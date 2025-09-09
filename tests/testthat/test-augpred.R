bd743818a2ad4300163ad1734583636bf98bd225
Matthew Fidler <matthew.fidler@gmail.com>
2025-09-09 11:50:53 -0500

    Apply pre-processing hooks for $simInfo and augPred(); test augPred

--- tests/testthat/test-augpred.R
+++ tests/testthat/test-augpred.R
@@ -197,4 +197,37 @@ nmTest({
 
     expect_error(augPred(fit), NA)
   })
+
+  test_that("augPred with zero etas", {
+
+    model.1compt.depot <- function() {
+      ini({
+        tka <- 0.45 # Log Ka
+        tcl <- 1  # Log Cl
+        tv <- 3.45  # Log V
+        eta.ka ~ 0.6
+        eta.cl ~ 0.3
+        eta.v ~ 0.1
+        add.sd <- 0.7
+      })
+      model({
+        ka <- exp(tka + eta.ka)
+        cl <- exp(tcl + eta.cl)
+        v <- exp(tv + eta.v)
+        d/dt(depot) = - ka*depot
+        d/dt(center) = ka*depot - cl/v*center
+        cp = center/v
+        cp ~ add(add.sd)
+      })
+    }
+
+    model.1compt.depot1 <- model.1compt.depot %>%
+      ini(eta.ka~0)
+
+    fit1 <- .nlmixr(model.1compt.depot1, theo_sd, est="focei",
+                    foceiControlFast)
+
+    expect_error(augPred(fit1), NA)
+
+  })
 })
