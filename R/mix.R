01835cc68b5a80657340e1563ebf7c68ff32e9a5
Matthew Fidler <matthew.fidler@gmail.com>
2025-09-26 14:48:43 -0500

    Add Ui components

---
+++ R/mix.R
@@ -0,0 +1,39 @@
+#' Get the mixture probabilities from the estimated log-scale parameters
+#'
+#' @param val numeric vector of the full parameter set in focei
+#'
+#' @param idx integer vector of the indices of the mixture log-scale
+#'   parameters
+#'
+#' @return A numeric vector of the mixture probabilities
+#'
+#' @noRd
+#'
+#' @author Matthew L. Fidler
+.getMixFromLog <- function(val, idx) {
+  v <- do.call("rxode2::mexpit", list(val[idx]))
+  c(v, 1-sum(v))
+}
+
+#' @export
+rxUiGet.thetaIniMix <- function(x, ...) {
+  .ui <- x[[1]]
+  .theta <- .ui$theta
+  if (length(.ui$mixProbs) > 0) {
+    .theta[.ui$mixProbs] <- rxode2::mlogit(.theta[.ui$mixProbs])
+  }
+  .theta
+}
+attr(rxUiGet.thetaIniMix, "rstudio") <- stats::setNames(1, "a")
+
+#' @export
+rxUiGet.thetaMixIndex <- function(x, ...) {
+  .ui <- x[[1]]
+  .theta <- .ui$theta
+  if (length(.ui$mixProbs) > 0) {
+    which(names(.ui$theta) %in% .ui$mixProbs)
+  } else {
+    integer(0)
+  }
+}
+attr(rxUiGet.thetaMixIndex, "rstudio") <- 1L
