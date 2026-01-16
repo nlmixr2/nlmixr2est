15c105bad9412e639e27173266cd5fa755226d6e
Matthew Fidler <matthew.fidler@gmail.com>
2026-01-15 19:13:05 -0600

    qs skip

--- tests/testthat/test-nlmixr3-load.R
+++ tests/testthat/test-nlmixr3-load.R
@@ -1,13 +1,11 @@
 nmTest({
   test_that("nlmixr3 fit loads", {
-
+    skip_if_not_installed("qs")
     fit <- readRDS(system.file("testfit_nlmixr3.rds", package = "nlmixr2est"))
 
     expect_error(rxode2::rxSolve(fit))
 
-    expect_snapshot(
-      fit <- suppressMessages(nlmixr2fix(fit))
-    )
+    fit <- suppressMessages(nlmixr2fix(fit))
 
     expect_error(rxode2::rxSolve(fit), NA)
 
