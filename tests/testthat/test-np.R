9a7c12a0609caff4c23edaf2f0553b5fff83a6af
mattfidler <matthew.fidler@gmail.com>
2026-05-29 11:40:32 -0500

    Update np to not run on CRAN

--- tests/testthat/test-np.R
+++ tests/testthat/test-np.R
@@ -81,34 +81,35 @@ nmTest({
     expect_error(.nlmixr(one.cmt, nlmixr2data::theo_sd, "focei", list(print=0)), NA)
     expect_error(.nlmixr(one.cmt, nlmixr2data::theo_sd, "saem", control = saemControlFast), NA)
   })
-})
 
-test_that("parameters are updated in fit object", {
+  test_that("parameters are updated in fit object", {
 
-  one.cmt <- function() {
-    ini({
-      ## You may label each parameter with a comment
-      tka <- 0.45 # Ka
-      tcl <- log(c(0, 2.7, 100)) # Log Cl
-      ## This works with interactive models
-      ## You may also label the preceding line with label("label text")
-      tv <- 3.45; label("log V")
-      ## the label("Label name") works with all models
-      add.sd <- 0.7
-    })
-    model({
-      ka <- exp(tka)
-      cl <- exp(tcl)
-      v <- exp(tv)
-      linCmt() ~ add(add.sd)
-    })
-  }
+    one.cmt <- function() {
+      ini({
+        ## You may label each parameter with a comment
+        tka <- 0.45 # Ka
+        tcl <- log(c(0, 2.7, 100)) # Log Cl
+        ## This works with interactive models
+        ## You may also label the preceding line with label("label text")
+        tv <- 3.45; label("log V")
+        ## the label("Label name") works with all models
+        add.sd <- 0.7
+      })
+      model({
+        ka <- exp(tka)
+        cl <- exp(tcl)
+        v <- exp(tv)
+        linCmt() ~ add(add.sd)
+      })
+    }
 
-  fit <- suppressMessages(suppressWarnings(.nlmixr(one.cmt, nlmixr2data::theo_sd, "focei",
-                                                  list(print=0))))
+    fit <- suppressMessages(suppressWarnings(.nlmixr(one.cmt, nlmixr2data::theo_sd, "focei",
+                                                     list(print=0))))
 
-  f1 <- suppressMessages(one.cmt())
+    f1 <- suppressMessages(one.cmt())
 
-  expect_false(isTRUE(all.equal(f1$iniDf$est, fit$iniDf$est)))
+    expect_false(isTRUE(all.equal(f1$iniDf$est, fit$iniDf$est)))
+
+  })
 
 })
