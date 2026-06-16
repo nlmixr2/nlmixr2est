b8473e6cf17816c856134322bca8ceea532bcb45
Bill Denney <wdenney@humanpredictions.com>
2026-06-16 15:35:21 -0400

    Remove functions and limits relating to old R and package versions

--- R/utils.R
+++ R/utils.R
@@ -244,21 +244,6 @@ cholSE <- function(matrix, tol = (.Machine$double.eps)^(1 / 3)) {
   setwd("c:/")
 }
 
-#' Generate a data.frame using the R4.0 convention
-#'
-#' @param ... Passed to \code{base::data.frame()} or
-#'   \code{base::as.data.frame()}
-#' @param stringsAsFactors Captured so that it can be ignored and always set to
-#'   \code{FALSE}
-#' @return A data.frame with strings not converted to factors
-#' @noRd
-.data.frame <- function(..., stringsAsFactors = FALSE) {
-  base::data.frame(..., stringsAsFactors = FALSE)
-}
-.as.data.frame <- function(..., stringsAsFactors = FALSE) {
-  base::as.data.frame(..., stringsAsFactors = FALSE)
-}
-
 #' Nelder-Mead simplex search
 #'
 #' @param start initials
